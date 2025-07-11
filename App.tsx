cd78b6481eef623cb624d50e71513fa5bc4bcfd7
vuhahodo <dophucvuha@gmail.com>
2025-07-11 13:47:21 +0700

    Khởi tạo lại project đầy đủ

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.editText}>I Love U</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  editText: {
+    fontSize: 40,
+    color: 'pink',
+    fontWeight: 'bold',
+  }
+});
