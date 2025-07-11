8c80604b9483ba0633fb49d916ca44f72c6384a9
vuhahodo <dophucvuha@gmail.com>
2025-07-11 13:51:27 +0700

    react-native Hello World

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style ={styles.editText}>I Love U</Text>
+      <Text style ={styles.editText}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
