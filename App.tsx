a6f821f2734506dede01b1479d687f02971c8e75
vuhahodo <dophucvuha@gmail.com>
2025-07-16 22:01:51 +0700

    This is Button

--- App.tsx
+++ App.tsx
@@ -1,25 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+
+  const quotes = [
+    "Yêu",
+    "Không",
+    "Khog biet",
+  ];
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
+  const [quote,setQuote] = useState<string>("Yêu nhau nha")
+  function handlePress() {
+    setCount(0);
+  }
+  function handlePressLove() {
+    const randomIndex = Math.floor(Math.random() * quotes.length)
+    setQuote(quotes[randomIndex]);
+  }  
+  
   return (
     <View style={styles.container}>
-      <Text style ={styles.editText}>Hello World</Text>
+      <Text style ={styles.test}>Hello World</Text>
+      <Text style ={styles.editText}>What the helk</Text>
+      <Button
+        title={count.toString()}
+        onPress={() => setCount(count + 1)}
+      />
+      <View style={{ marginTop: 20 , marginBottom: 20 }}>
+        <Button 
+          color={"red"}
+          title="Reset Count"
+          onPress={handlePress}
+        />
+      </View>
+      <View>
+        <Text style={styles.Text}>Yêu hay không </Text>
+        <Button
+          title  = {quote}
+          color = "pink"
+          onPress={handlePressLove}
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+
+  },
   editText: {
     fontSize: 40,
     color: 'pink',
     fontWeight: 'bold',
+  },
+  test: {
+    fontSize: 20,
+    color: 'blue',
+    fontWeight: 'normal',
   }
 });
